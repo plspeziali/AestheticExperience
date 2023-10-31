ddad2f1e3975f99ba10992821b1d8e2fd7393290
Paolo Speziali <speziali99@gmail.com>
2023-10-31 21:21:28 +0100

    The icon plus name combo works

--- script.js
+++ script.js
@@ -3,7 +3,29 @@ const paintingsList = ["Klee Yellow", "Klee Red",
                                 "Kandinsky Albertina", "Kandinsky Pompidou",
                                 "Mortensen Pink", "Mortensen Orange",
                                 "Miro", "Winter"]
+const adjectiveToIcon = {
+    Positive: "sentiment_very_satisfied",
+    Active: "directions_run",
+    Still: "free_breakfast",
+    Sad: "sentiment_very_dissatisfied",
+    Peaceful: "spa",
+    Hard: "accessibility",
+    Cold: "ac_unit",
+    Light: "wb_sunny",
+    Rough: "terrain",
+    Spiritual: "brightness_2",
+    Feminine: "pregnant_woman",
+    Cautious: "security",
+    Like: "thumb_up",
+    Interesting: "emoji_objects",
+};
 const paintingsLength = paintingsList.length;
+
+// JavaScript to inhibit scrolling down and disable the right scrollbar
+window.addEventListener('scroll', function(e) {
+    window.scrollTo(0, 0); // Disable vertical scrolling
+    e.preventDefault(); // Prevent default scrolling behavior
+});
 
 am4core.ready(function() {
 
@@ -50,56 +72,11 @@ function createChart(paint_selected){
 
         let paint_obj = [{
             name: paintingsList[paint_selected],
-            children: json_data[paintingsList[paint_selected]]
+            children: json_data[paintingsList[paint_selected]],
         }]
 
-        function minMaxNormalizeToRange(data, newMin, newMax) {
-            let minValue = Number.POSITIVE_INFINITY;
-            let maxValue = Number.NEGATIVE_INFINITY;
-
-            // Recursive function to traverse the JSON data structure
-            function traverse(obj) {
-                for (const obj of data) {
-                    console.log(obj)
-                    for (const emo of obj.children) {
-                        console.log(emo)
-                        for (const emo2 of emo.children) {
-                             if (typeof emo2.value === 'number') {
-                                // If the property is a number, update min and max values
-                                const value = emo2.value;
-                                minValue = Math.min(minValue, value);
-                                maxValue = Math.max(maxValue, value);
-                            }
-                        }
-                    }
-                }
-            }
-
-            traverse(data);
-
-            // Calculate the range of the original data
-            const range = maxValue - minValue;
-
-            // Perform min-max normalization
-            for (const obj of data) {
-                console.log(obj)
-                for (const emo of obj.children) {
-                    console.log(emo)
-                    for (const emo2 of emo.children) {
-                        console.log(emo2)
-                        if (typeof emo2.value === 'number') {
-                            emo2.oldValue = emo2.value
-                            emo2.value = (emo2.value - minValue) / range * (newMax - newMin) + newMin;
-                        }
-                    }
-                }
-            }
-
-            return { min: newMin, max: newMax };
-        }
-
         // Define the new min and max values for scaling (20-40)
-        const newMinValue = 10;
+        const newMinValue = 30;
         const newMaxValue = 50;
 
         // Normalize to the 20-40 scale and find the new min and max values
@@ -111,50 +88,90 @@ function createChart(paint_selected){
 
         networkSeries.dataFields.value = "value";
         networkSeries.dataFields.name = "name";
+        networkSeries.dataFields.icon = "icon"
         networkSeries.dataFields.children = "children";
         networkSeries.nodes.template.tooltipText = "{name}";
         networkSeries.nodes.template.fillOpacity = 1;
-        networkSeries.nodes.template.label.text = "{name}";
-        networkSeries.fontSize = 10;
-        networkSeries.centerStrength = 0.5;
 
-        // Define node template
-        var nodeTemplate = networkSeries.nodes.template;
-        nodeTemplate.propertyFields.value = "value"; // Set node radius based on 'value' property
-        nodeTemplate.radius = 20; // Default node radius
-        nodeTemplate.fillOpacity = 0.7;
+        //networkSeries.nodes.template.label.text = "{name}"
 
-// Create the label
-        var label = nodeTemplate.createChild(am4core.Label);
-        label.text = "{oldValue}";
-        label.fill = am4core.color("#FFFFFF"); // White text color
-        label.fontSize = 12;
-        label.dy = -10; // Adjust the label's vertical position
-        nodeTemplate.events.on("ready", function (event) {
-            label.hide();
-        });
-
-        // Define links between nodes if necessary
-        networkSeries.links.template.distance = 1;
-
-        // Enable drag and zoom interaction
-        chart.zoomable = true;
-        chart.seriesContainer.draggable = true;
-
-        // Add a click event to show/hide the label
-        nodeTemplate.events.on("hit", function (event) {
-            var clickedNode = event.target;
-            if (label.isHiding || label.isHidden) {
-                label.show();
-            } else {
-                label.hide();
+        networkSeries.nodes.template.label.html = "<i class=\"material-icons\">{icon}</i>";
+        // Modify the node template for nodes at depth 1
+        networkSeries.nodes.template.adapter.add("label.html", function(html, target) {
+            console.log(target)
+            if (target.dataItem.dataContext.text === paintingsList[paint_selected]) {
+                // Add custom HTML for nodes with matching text
+                return `<div>ciaa</div>`;
             }
+            return html; // Use the default HTML for other nodes
+        });
+        networkSeries.nodes.template.label.adapter.add("html", function(text, target) {
+            if (target.dataItem) {
+                switch(target.dataItem.level) {
+                    case 0:
+                        return "<h5>{name}</h5>";
+                }
+            }
+            return text;
         });
 
-        // Create a black outline for the label
-        label.outline = true;
-        label.outlineWidth = 2;
-        label.outlineColor = am4core.color("#000");
+
+        networkSeries.nodes.template.label.adapter.add("fill", function(fill, target) {
+            if (target.dataItem.level > 1) {
+                return am4core.color("#000");
+            }
+            return fill;
+        })
+
+        networkSeries.nodes.template.adapter.add("tooltipText", function (text, target) {
+            var node = target;
+            if (node.dataItem.level === 2) {
+                // Customize the tooltip text for nodes at the third depth
+                return "{name}: {oldValue}";
+            }
+            return text;
+        });
+
+        networkSeries.nodes.template.label.adapter.add("fontSize", function(fontSize, target) {
+            var node = target;
+            if (node.dataItem.level === 2) {
+                return 12; // Customize the font size for nodes at level 2
+            }
+            return 16; // Keep the default font size for other nodes
+        });
+
+        // Customize minRadius for nodes at the first level depth
+        networkSeries.nodes.template.paddingRadius = 20
+        networkSeries.nodes.template.innerWidth = 60
+
+
+        networkSeries.fontSize = 10;
+
+        networkSeries.links.template.strokeWidth = 1;
+
+        var hoverState = networkSeries.links.template.states.create("hover");
+        hoverState.properties.strokeWidth = 3;
+        hoverState.properties.strokeOpacity = 1;
+
+        networkSeries.nodes.template.events.on("over", function(event) {
+            event.target.dataItem.childLinks.each(function(link) {
+                link.isHover = true;
+            })
+            if (event.target.dataItem.parentLink) {
+                event.target.dataItem.parentLink.isHover = true;
+            }
+
+        })
+
+        networkSeries.nodes.template.events.on("out", function(event) {
+            event.target.dataItem.childLinks.each(function(link) {
+                link.isHover = false;
+            })
+            if (event.target.dataItem.parentLink) {
+                event.target.dataItem.parentLink.isHover = false;
+            }
+        })
+
 
         // Define links between nodes if necessary
         networkSeries.links.template.distance = 1;
@@ -202,3 +219,52 @@ nextButton.addEventListener('click', () => {
 
 // Initial rendering
 renderCurrentChart();
+
+
+function minMaxNormalizeToRange(data, newMin, newMax) {
+    let minValue = Number.POSITIVE_INFINITY;
+    let maxValue = Number.NEGATIVE_INFINITY;
+
+    // Recursive function to traverse the JSON data structure
+    function traverse(obj) {
+        for (const obj of data) {
+            //console.log(obj)
+            for (const emo of obj.children) {
+                emo.icon = adjectiveToIcon[emo.name];
+                //console.log(emo)
+                for (const emo2 of emo.children) {
+                    emo2.icon = adjectiveToIcon[emo2.name];
+                    if (typeof emo2.value === 'number') {
+                        // If the property is a number, update min and max values
+                        const value = emo2.value;
+                        minValue = Math.min(minValue, value);
+                        maxValue = Math.max(maxValue, value);
+                    }
+                }
+            }
+        }
+    }
+
+    traverse(data);
+
+    // Calculate the range of the original data
+    const range = maxValue - minValue;
+
+    // Perform min-max normalization
+    for (const obj of data) {
+        //console.log(obj)
+        for (const emo of obj.children) {
+            //console.log(emo)
+            for (const emo2 of emo.children) {
+                //console.log(emo2)
+                if (typeof emo2.value === 'number') {
+                    emo2.oldValue = emo2.value
+                    emo2.value = (emo2.value - minValue) / range * (newMax - newMin) + newMin;
+
+                }
+            }
+        }
+    }
+
+    return { min: newMin, max: newMax };
+}
